bc5ad0f9526d2eccf4b52408b78ad9fb86e891af
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-04-13 15:13:04 +0530

    worked on navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { Hero, Navbar, About, Tech, Experience, Works, Feedbacks, Contact, StarsCanvas } from './components';
+
 const App = () => {
 
   return (
-    <div>
-      Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
