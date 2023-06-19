713afd3107d4e48d4c63de55b747d3560be4b3a4
sidd84755 <sidd84755@gmail.com>
2023-06-19 21:37:28 +0530

    Worked on U.I

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
+import earth from "../assets/earth.png"
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
@@ -66,7 +67,7 @@ const Contact = () => {
 
   return (
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 items-center overflow-hidden`}
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
@@ -127,7 +128,11 @@ const Contact = () => {
         variants={slideIn("right", "tween", 0.2, 1)}
         className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
       >
-        <EarthCanvas />
+        {/* <EarthCanvas /> */}
+        <div className="ea1">
+          <img src={earth} alt="" />
+        </div>
+        
       </motion.div>
     </div>
   );
